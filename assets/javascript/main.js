43cfdb59b8c2d78c7efcbae8c6c027ca81c33bc3
Kevin Schroeder <schroederkevin94@gmail.com>
2019-03-28 16:32:12 -0400

    WIP added click function to play gif

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,15 @@
 let topics = ['celebrate', 'anger', 'sad', 'dance', 'confused', 'fail', 'sucess'];
 let queryURL = '';
 let imgSource = '';
-let gif = '';
+let staticGif = '';
+let movingGif = '';
+let staticURL = '';
+let movingURL = '';
+let currentURL = '';
+
 
 //Create buttons
-for (let i = 0; i < topics.length; i++){
+for (let i = 0; i < topics.length; i++) {
     $('<button type="button" class = "topic btn-primary btn">').attr("id", topics[i]).text(topics[i]).appendTo('#topic-row');
 }
 
@@ -23,7 +28,7 @@ $('.topic').on('click', function () {
     getGif();
 })
 
-$('.topic').hover(moveGif);
+
 
 function getGif() {
     $.ajax({
@@ -32,34 +37,58 @@ function getGif() {
     }).then(function (response) {
         //TEST
         console.log(response);
+
         //Display 10 gifs in a div inside the gif-row
         for (let j = 0; j < 10; j++) {
-            let rating = response.data[j].rating;
+            //Get rating for image
+            //Make a paragraph with the correct rating
+            let rating = $('<p class = "rating">').text("Rating: " + response.data[j].rating);
 
-            let ratingText = $('<p class = "rating">').text("Rating: " + rating);
+            //Set image source to the still Gif image
+            staticUrl = response.data[j].images.fixed_width_still.url;
+            movingUrl = response.data[j].images.fixed_width.url;
 
-            imgSource = response.data[j].images.fixed_width_still.url;
-            gif = $('<img class = "gif" src = ' + imgSource + ' > ');
+            //Create an image element and put the source of the still image in there
+            staticGif = $('<img class = "gif" src = ' + staticUrl + ' data-moving =' + movingUrl + ' data-static = ' + staticUrl + ' data-state = "static" > ');
 
-            $('<div class = "col-3">').append(ratingText).append(gif).insertAfter('#gif-row');
+            //Create a div with the rating and the gif inside
+            $('<div class = "col-3">').append(rating).append(staticGif).insertAfter('#gif-row');
+        
 
         }
     });
+
+
 };
 
-function moveGif() {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        for (let h = 0; h < 10; h++) {
-            if (response.data[j])
-            imgSource = response.data[j].images.fixed_width_still.url;
-            gif = $('<img class = "gif" src = ' + imgSource + ' > ');
-            this.removeAttr('src');
-            this.addAttr('src')
 
-        }
+$(document).on('click','.gif', function (){
+    let state = $(this).attr('data-state');
+    console.log(state);
+
+    if (state === 'static') {
+        $(this).attr('src', $(this).attr('data-moving'));
+        $(this).attr('data-state', 'moving');
+    } else{
+        $(this).attr('src', $(this).attr('data-static'));
+        $(this).attr('data-state', 'static');
     }
-    });
-};
+});
+
+// function moveGif() {
+//     $('.static-gif').hide();
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         for (let j = 0; j < 10; j++) {
+//             //Set image source to the still Gif image
+//             let movingImgSource = response.data[j].images.fixed_width.url;
+//             //Create an image element and put the source of the still image in there
+//             let movingGif = $('<img class = "moving-gif" src = ' + movingImgSource + ' > ');
+//             //Create a div with the rating and the gif inside
+//             $('<div class = "col-3">').append(ratingText).append(movingGif).insertAfter('#gif-row');
+//         }
+
+//     });
+// };
